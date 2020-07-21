ff61f07a9c4519331fe959e01cfafd58fc82ae07
LordCommander21 <gogadzedaniel21@gmail.com>
2020-07-21 21:13:33 +0400

    parser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Document page = Jsoup.parse(new URL("https://amindi.ge/"),3000);
     }
 }
